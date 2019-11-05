41695755c48555b3c6a82889d68dbd14db8b9c0f
Atick45 <atikbakshi45@gmail.com>
2019-11-05 21:21:35 +0600

    Third Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,7 +82,7 @@ class UserController extends Controller
         $user->email = $request->input('email');
         $user->password = Hash::make($request->input('password'));
         $user->role_id = $request->input('role_id');
-        $user->dept_id = $request->input('department_id');
+        $user->department_id = $request->input('department_id');
         $user->picture = $fileNameToStore;
         $user->save();
         return redirect('/user')->with('success', 'User Added Successfull'); 
